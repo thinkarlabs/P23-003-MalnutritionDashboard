c3c4d7a8bfe6be96c395b9d84608e134fb4a86fc
Jaswinder Singh <findjassi121212@gmail.com>
2023-03-07 13:23:34 +0530

    Updated changes in index.js

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -19,17 +19,11 @@ const router = createRouter({
     {
       path: "/addngo",
       name: "addngo",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AddNgoView.vue"),
     },
     {
       path: "/kidsCheckUp",
       name:"kidsCheckUp",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/kidsCheckUp.vue"),
     },
     {
