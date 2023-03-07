c2a87069d0839ca4130f2d7a3fb18d96fa52c28e
Jaswinder Singh <findjassi121212@gmail.com>
2023-03-07 12:26:51 +0530

    changes in main files

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Backend.routes.routes import user_router
 from Backend.routes.routes import ngo_router, aanganwadi_router, child_router
 from Backend.routes.routes import donor_router
 
-# from app.routes import sign_router
+from app.routes import sign_router
 
 
 
@@ -37,10 +37,9 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# app.include_router(sign_router)
+app.include_router(sign_router)
 app.include_router(user_router)
 
-# app.include_router(sign_router)
 app.include_router(ngo_router)
 app.include_router(donor_router)
 app.include_router(aanganwadi_router)
