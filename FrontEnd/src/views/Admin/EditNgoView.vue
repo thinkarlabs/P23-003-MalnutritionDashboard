0253cd8e9825b68b90ddfa5cefbb2291117feed1
mjayakumarmca <mjayakumarmca@gmail.com>
2023-04-06 16:44:08 +0530

    fixed rebase issues

--- FrontEnd/src/views/Admin/EditNgoView.vue
+++ FrontEnd/src/views/Admin/EditNgoView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="full-div container" style="width: 1280px">
     <div id="x-contest" class="container-fluid p-3">
-      <form @submit.prevent="updateNgo">
+      <form v-on:submit.prevent="updateNgo">
         <div class="row">
           <h3 class="float-start">Manage NGO</h3>
 
@@ -14,6 +14,9 @@
               placeholder="Ngo Name"
               v-model="updatedNgo.ngoName"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.ngoName">
+              {{ helperSupport.ngoName }}
+            </div>
           </div>
 
           <div class="col-6 my-2">
@@ -25,6 +28,9 @@
               placeholder="Contact Person Name"
               v-model="updatedNgo.contactPersonName"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.contactPersonName">
+              {{ helperSupport.contactPersonName }}
+            </div>
           </div>
 
           <div class="col-6 my-2">
@@ -36,6 +42,9 @@
               placeholder="Contact Person Phone"
               v-model="updatedNgo.contactPersonPhone"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.contactPersonName">
+              {{ helperSupport.contactPersonName }}
+            </div>
           </div>
 
           <div class="col-6 my-2">
@@ -47,6 +56,9 @@
               placeholder="Contact Person Email"
               v-model="updatedNgo.contactPersonEmail"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.contactPersonEmail">
+              {{ helperSupport.contactPersonEmail }}
+            </div>
           </div>
 
           <div class="col-6 my-2">
@@ -58,6 +70,12 @@
               placeholder="Password"
               v-model="updatedNgo.contactPersonPassword"
             />
+            <div
+              className="text-danger mrgnbtn"
+              v-if="helperSupport.contactPersonPassword"
+            >
+              {{ helperSupport.contactPersonPassword }}
+            </div>
           </div>
           <div class="col-6 my-2">
             <label for="exampleFormControlInput1">Location</label>
@@ -68,6 +86,9 @@
               placeholder="Location"
               v-model="updatedNgo.location"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.location">
+              {{ helperSupport.location }}
+            </div>
           </div>
           <div class="col-6 my-2">
             <label for="exampleFormControlInput1">Pincode</label>
@@ -78,6 +99,9 @@
               placeholder="pincode"
               v-model="updatedNgo.pincode"
             />
+            <div className="text-danger mrgnbtn" v-if="helperSupport.pincode">
+              {{ helperSupport.pincode }}
+            </div>
           </div>
           <div class="row">
             <div class="col-12 p-2">
@@ -134,16 +158,17 @@ import { ref, onMounted, computed, reactive } from "vue";
 import { useNgoStore } from "../stores/ngo";
 import { useRoute } from "vue-router";
 import router from "../router";
+import helper from "../helper/validation.helper.js";
 
 let updatedNgo = reactive({
   id: "",
   ngoName: "",
   contactPersonName: "",
   contactPersonEmail: "",
-  contactPersonPhone: 0,
+  contactPersonPhone: "",
   contactPersonPassword: "",
   location: "",
-  pincode: 0,
+  pincode: "",
 });
 
 const route = useRoute();
@@ -165,6 +190,35 @@ updatedNgo = computed(() => {
     };
   }
 });
+
+const helperSupport = reactive({
+  ngoName: "",
+  contactPersonName: "",
+  contactPersonEmail: "",
+  contactPersonPhone: "",
+  contactPersonPassword: "",
+  location: "",
+  pincode: "",
+});
+
+const isValidSubmission = (updatedNgo) => {
+  helperSupport.pincode = helper.validatePincode(updatedNgo._value.pincode);
+  helperSupport.contactPersonName = helper.validateName(
+    updatedNgo._value.contactPersonName
+  );
+  helperSupport.contactPersonEmail = helper.validateEmail(
+    updatedNgo._value.contactPersonEmail
+  );
+  helperSupport.ngoName = helper.validateName(updatedNgo._value.ngoName);
+  helperSupport.contactPersonPhone = helper.validatePhoneNumber(
+    updatedNgo._value.contactPersonPhone
+  );
+  helperSupport.location = helper.validateName(updatedNgo._value.location);
+  helperSupport.contactPersonPassword =
+    updatedNgo._value.contactPersonPassword !== "" ? "" : "Password is mandatory";
+  return helper.isErrorMessagesAvailable(helperSupport) ? false : true;
+};
+
 onMounted(async () => {
   console.log("Editing.... ");
   console.log(route.params.id);
@@ -173,7 +227,9 @@ onMounted(async () => {
 });
 
 const updateNgo = async () => {
-  await store.updateNgo(updatedNgo);
-  return router.push("/ngos");
+  if (isValidSubmission(updatedNgo) == true) {
+    await store.updateNgo(updatedNgo);
+    return router.push("/ngos");
+  }
 };
 </script>
