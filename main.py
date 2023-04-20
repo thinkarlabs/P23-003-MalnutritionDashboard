423ae4f3c70f3a98524c0808c45d4226a7f46882
Ahmed <786.aha91@gmail.com>
2023-04-20 15:41:07 +0530

    Cypress e2e Testing for Login. Added an id in the LoginWebView.vue for cypress testing. localhost added for CORS issue for cypress testing.

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173", "http://localhost:5173/",
                    "http://localhost:8080", "http://localhost:8080/",
                    "http://127.0.0.1:8887", "http://127.0.0.1:8887/",
-                   "http://127.0.0.1:4173", "http://127.0.0.1:4173/",
+                   "http://localhost:4173", "http://127.0.0.1:4173/",
                    "http://localhost:7000", "http://localhost:7000/"],
     allow_credentials=True,
     allow_methods=["*"],
